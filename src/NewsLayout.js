e4e62da808fcabe90beb2517afaf808c8706f710
hashZero1 <117970097+hashZero1@users.noreply.github.com>
2023-04-09 15:31:18 +0530

    Add files via upload

--- src/NewsLayout.js
+++ src/NewsLayout.js
@@ -1,87 +1,54 @@
 import * as React from 'react';
-import {Card,Link,Divider} from '@mui/material';
+import { Link, Divider} from '@mui/material';
 import CardHeader from '@mui/material/CardHeader';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import {Button} from '@mui/material';
-import Fade from '@mui/material/Fade';
-import CardActions from '@mui/material/CardActions';
 import Typography from '@mui/material/Typography';
-const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  bgcolor: 'background.paper',
-  borderRadius:4,
-  boxShadow: 10,
-  p: 10,
-};
 
 export default function RecipeReviewCard({news}) {
-  const [open, setOpen] = React.useState(false);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
   return (
-    <Card sx={{ maxWidth: 400 ,borderRadius: 2}}>
+    <>
+      <CardMedia
+           sx={{borderRadius: '5px'}}
+        component="img"
+        height="600"
+        image={news.urlToImage}
+        alt={news.urlToImage}
+      />
       <CardHeader
         title={news.author}
         subheader={news.publishedAt}
       />
-      <CardMedia
-        component="img"
-        height="294"
-        image={news.urlToImage}
-        alt="Paella dish"
-      />
-      <CardContent >
-      <Typography  sx={{margin:1}} variant="body2" color="text.secondary">
-        {news.source.name}
+      <CardContent>
+      <Typography variant="h6" color="text.secondary">
+      {news.title}
         </Typography>
-        <Typography sx={{margin:1}} variant="body1" color="text.secondary">
-        {news.title}
+        <Typography variant="body2" color="text.secondary">
+        Source: {news.source.name}
         </Typography>
-     
       </CardContent>
-      <CardActions disableSpacing>
-        <Button onClick={handleOpen}>Show Details</Button>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        slotProps={{
-          backdrop: {
-            timeout: 500,
-          },
-        }}
-      >
-        <Fade in={open}>
-          <Box sx={style}>
-            <CardMedia sx={{
-                marginBottom:'10px'}}
-              component="img"
-              height="294"
-              image={news.urlToImage}
-              alt="Paella dish"
-              />
-             <Typography sx={{marginBottom:'10px'}} id="transition-modal-title" variant="h6" component="h2">
-               {news.description}
-             </Typography>
-             <Divider/>
-             <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-               {news.content}
-             </Typography>
-            <Link variant='h6' href={news.url}>Read full article here</Link>
-          </Box>
-        </Fade>
-      </Modal>
-      </CardActions>
-      </Card>
+      <Divider/>
+        <CardContent>
+          <Typography variant='h6' color="text.secondary" paragraph>Content</Typography>
+          <Typography paragraph>
+          {news.description}
+          </Typography>
+          <Typography paragraph>
+          {news.content}
+          </Typography>
+          
+          <Typography paragraph>
+            Add rice and stir very gently to distribute. Top with artichokes and
+            peppers, and cook without stirring, until most of the liquid is absorbed,
+            15 to 18 minutes. Reduce heat to medium-low, add reserved shrimp and
+            mussels, tucking them down into the rice, and cook again without
+            stirring, until mussels have opened and rice is just tender, 5 to 7
+            minutes more. (Discard any mussels that don&apos;t open.)
+          </Typography>
+          <Link variant='h6' href={news.url}>Read full article here</Link>
+        </CardContent>
+     
+    </>
+         
   );
 }
-
-
--- src/NewsLetter.js
+++ src/NewsLetter.js
@@ -2,17 +2,21 @@ import React, {useState, useEffect } from 'react';
 import axios from 'axios';
 import NewsLayout from './NewsLayout';
 import SearchAppBar from './Appbar';
-import { Grid,Paper} from '@mui/material';
+import { Grid,Paper, Typography, Divider} from '@mui/material';
 function NewsLetter() {
     const [newState, setState] =useState();
     useEffect(() => {
-        async function getData() {
+        try{
+            async function getData() {
             const response = await axios.get('https://newsapi.org/v2/everything?domains=wsj.com&apiKey=9fcfe29cb7da42939d6ac3c7a92742a4');
             const data = response.data.articles;
             console.log(data);
             setState(data);
         }
         getData();
+        }catch(err){
+            console.log(err)
+        }
     }, [])
     return (
         <Paper sx={{ backgroundImage: `url(${Image})` }} style={{
@@ -21,15 +25,21 @@ function NewsLetter() {
             backgroundColor: "#fafafa"
         }}elevation ={0}>
             <SearchAppBar/>
+            <Typography sx={{ 
+                margin:'20px',
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',}} variant='h2' >Today's News Headlines</Typography>
+                <Divider/>
             <Grid container  sx={{
                 marginTop:'10px',
                 padding:'10px',
                 display: 'flex',
                 justifyContent: 'center',
                 alignItems: 'center',
-            }} spacing={4}>
+            }} spacing={2}>
             {newState && newState.map((news) => (
-                <Grid item >
+                <Grid item xs={12} md={8}>
                      <NewsLayout news={news}/>
                 </Grid>
             ))}
